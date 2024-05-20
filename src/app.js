72a1e7946330d7fa01f30b2a1ed0551c960c4d43
AlexisRSM <alexismendes98@hotmail.com>
2024-05-20 18:22:21 +0100

    Finalized Project

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
 /* eslint-disable */
-import "bootstrap";
+/* import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+import "./assets/img/4geeks.ico"; */
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+function something() {
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+  let extension=['.com','.pt','doggie','.net','.org','.io','.gov','.edu','.mil','.int','.biz','.info','.name','.pro','.coop','.aero','.museum','.cat','.jobs','.mobi','.travel','.tel','.xxx','.asia','.moe','.post','.geo','.kid','.mail','.ninja','.party','.photo','.pics','.science','.site','.space','.tech','.uno','.video','.wiki','.world','.zone','.academy','.accountant','.actor','.agency','.airforce','.army','.associates','.attorney','.auction','.band','.bar','.bargains','.beer','.best','.bid','.bike','.bingo','.bio','.black','.blackfriday'];
+ 
+  let pronounId = Math.floor(Math.random()*pronoun.length);
+  let adjId = Math.floor(Math.random()*adj.length);
+  let nounId = Math.floor(Math.random()*noun.length);
+  let extensionId = Math.floor(Math.random()*extension.length);
+
+  console.log(pronoun[pronounId] + adj[adjId] + noun[nounId]+ extension[extensionId]);
 };
+something();
